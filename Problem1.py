b43ded1c2adc8439a83f90a5f4d3f4ed8de0b79a
Jarrod Ozereko <jarrod.oz@gmail.com>
2026-02-16 16:48:58 -0800

    Add greeting bot implementation in Problem1.py
    Implement a greeting bot that introduces itself and its creation year.

---
+++ Problem1.py
@@ -0,0 +1,13 @@
+##Objective
+##For the first stage, you will write a bot that displays a greeting, its name, and the year it was created. First impressions count!
+##Your program should print the following lines:
+##Hello! My name is {bot_name}.
+##I was created in {birth_year}.
+##Instead of {bot_name}, print any name you choose and replace {birth_year} with the current year (four digits). You don't need to take any input at this stage.
+
+#Define variables for later use
+bot_name = 'Mycroft'
+birth_year = 2026
+
+print(f"Hello! My name is {bot_name}.")
+print(f"I was created in {str(birth_year)}.")
